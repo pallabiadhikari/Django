66854061c72fea5607c4be7d590f6b845699b423
Pallabi-62 <pallabiadhikari62@gmail.com>
2025-07-22 20:19:12 +0545

    Completed

--- quiz_platform/settings.py
+++ quiz_platform/settings.py
@@ -119,7 +119,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = BASE_DIR /'staticfiles'
 STATIC_ROOT=os.path.join(BASE_DIR,'staticfiles')
 STATICFILES_STORAGE='whitenoise.storage.CompressManifestStaticFilesStorage'
 MEDIA_URL='/media/'
